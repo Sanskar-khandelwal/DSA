6e025cd3baa04fed7d309d6417b31252a93c89b1
Sanskar khandelwal <Sanskarkhandelwal336@gmail.com>
2023-01-14 22:29:30 +0530

    added a new method to check weather array is sorted or not

--- RecursionArray/IsSorted.java
+++ RecursionArray/IsSorted.java
@@ -1,33 +1,56 @@
+//package RecursionArray;
+//
+//public class IsSorted {
+//
+//    public static void main(String[] args) {
+//        int[] nums = {1,16,30, 32, 40};
+//
+//        boolean ans = isSorted(nums, 0);
+//        System.out.println(ans);
+//
+//
+//    }
+//
+//    public static boolean isSorted(int[] nums, int index) {
+//
+//
+//        // Naive linear Search Approach but we want to do it with recursion
+////        for (int i = 1; i < nums.length; i++) {
+////
+////            if (nums[i] < nums[i - 1]){
+////                {
+////                    return false;
+////                }
+////            }
+////        }
+////    return true;
+////    }
+//        if (index == nums.length - 1) {
+//            return true;
+//        }
+//        return nums[index] < nums[index + 1] & isSorted(nums, index+ 1);
+//    }
+//}
+
 package RecursionArray;
 
 public class IsSorted {
-
     public static void main(String[] args) {
-        int[] nums = {1,16,30, 32, 40};
-
-        boolean ans = isSorted(nums, 0);
+        int arr[] = { 1, 2, 3,  5,4 };
+        boolean ans = isSortedOrNot(arr, 0, 1);
         System.out.println(ans);
-
-
     }
 
-    public static boolean isSorted(int[] nums, int index) {
-
-
-        // Naive linear Search Approach but we want to do it with recursion
-//        for (int i = 1; i < nums.length; i++) {
-//
-//            if (nums[i] < nums[i - 1]){
-//                {
-//                    return false;
-//                }
-//            }
-//        }
-//    return true;
-//    }
-        if (index == nums.length - 1) {
+    public static boolean isSortedOrNot(int[] nums, int i, int j) {
+        int n = nums.length;
+        if (i == n -1) {
             return true;
         }
-        return nums[index] < nums[index + 1] & isSorted(nums, index+ 1);
+        if (nums[i] < nums[j]) {
+            return isSortedOrNot(nums, i + 1, j + 1);
+        }
+        return false;
+
     }
 }
+
