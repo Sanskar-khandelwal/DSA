cdfdab4e2c26eafcb7788a1280107c957432b178
Sanskar khandelwal <Sanskarkhandelwal336@gmail.com>
2023-01-18 21:29:57 +0530

    tried to solve 1 question

--- Practice/Solution.java
+++ Practice/Solution.java
@@ -1,19 +1,33 @@
 package Practice;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 class Solution {
-    public int maxValue(int n, int index, int maxSum) {
-        int maxNum = 0;
-        for(int i = 1; i < maxSum; i++){
-           maxNum = maxNum + i;
-           if(maxNum >= maxSum){
-               break;
-           }
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.
+        int[] nums = {1,2,3,7, 4,0};
+     int n =    mountainArray(nums);
+        System.out.println(n);
+    }
+
+    private static int mountainArray(int[] nums) {
+        int s = 0;
+        int e = nums.length -1;
+        while(s <= e){
+            int mid = s + (e-s)/2;
+            if(nums[mid-1] < nums[mid] && nums[mid] < nums[mid+1]){
+            s = mid + 1;
+            }
+            else if(nums[mid -1] < nums[mid] && nums[mid] > nums[mid+1]){
+                e = mid -1;
+            }
+            else{
+                return mid;
+            }
         }
-        if(index == n -1){
-            return maxNum;
-        }
-        else{
-            return maxNum -1;
-        }
+        return -1;
+
     }
 }
